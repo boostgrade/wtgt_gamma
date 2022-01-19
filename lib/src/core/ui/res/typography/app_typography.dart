94d9dbfa531976a839cabaf7253999a465fbb822
Dmitry Dudunov <dydynovdima6@gmail.com>
2022-01-19 20:07:01 +0300

    feat: init typography

--- lib/src/core/ui/res/typography/app_typography.dart
+++ lib/src/core/ui/res/typography/app_typography.dart
@@ -1,1 +1,40 @@
-abstract class AppTypography {}
+import 'package:flutter/material.dart';
+
+abstract class AppTypography {
+  static const TextStyle subtitle1 = TextStyle(
+    fontFamily: 'Montserrat500',
+    fontSize: 16,
+  );
+
+  static const TextStyle headline2 = TextStyle(
+    fontFamily: 'Montserrat600',
+    fontSize: 24,
+  );
+
+  static const TextStyle body1 = TextStyle(
+    fontFamily: 'Montserrat',
+    fontSize: 16,
+    letterSpacing: 0.08,
+  );
+
+  static const TextStyle body2 = TextStyle(
+    fontFamily: 'Montserrat',
+    fontSize: 18,
+    letterSpacing: 0.08,
+  );
+
+  static const TextStyle caption = TextStyle(
+    fontFamily: 'Montserrat',
+    fontSize: 16,
+  );
+
+  static const TextStyle label = TextStyle(
+    fontFamily: 'Montserrat',
+    fontSize: 12,
+  );
+
+  static const TextStyle promoBold = TextStyle(
+    fontFamily: 'Montserrat700',
+    fontSize: 16,
+  );
+}
