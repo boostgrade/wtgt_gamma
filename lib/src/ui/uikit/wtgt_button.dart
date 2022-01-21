5ba51e69d982c4467eb79b9b4fab6bdbc4bddc4b
Dmitriy Usatov <usatov.dv@gmail.com>
2022-01-21 23:53:31 +0300

    feat: init WtgtButton

--- lib/src/ui/uikit/wtgt_button.dart
+++ lib/src/ui/uikit/wtgt_button.dart
@@ -1,18 +1,28 @@
 import 'package:flutter/material.dart';
+import 'package:where_to_go_today/src/ui/uikit/wtgt_circular_progress_indicator.dart';
 
 class WtgtButton extends StatelessWidget {
+  final String label;
+  final VoidCallback? onPressed;
+  final bool loading;
+
   const WtgtButton({
     Key? key,
-    // ignore: avoid_unused_constructor_parameters
-    void Function()? onTap,
-    // ignore: avoid_unused_constructor_parameters
-    required Widget child,
-    // ignore: avoid_unused_constructor_parameters
-    required bool loading,
+    required this.label,
+    this.onPressed,
+    this.loading = false,
   }) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return TextButton(
+      onPressed: loading ? null : onPressed,
+      child: loading
+          ? const WtgtCircularProgressIndicator()
+          : Text(
+              label,
+              overflow: TextOverflow.ellipsis,
+            ),
+    );
   }
 }
