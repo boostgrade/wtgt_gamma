ba8f91f63a9d9d917e9c6b17750cb9eb4359513f
Dmitry Dudunov <dydynovdima6@gmail.com>
2022-01-29 20:58:33 +0300

    fix: main route

--- lib/src/features/main/main_screen_route.dart
+++ lib/src/features/main/main_screen_route.dart
@@ -1,23 +1,20 @@
 import 'package:flutter/material.dart';
-// import 'package:provider/provider.dart';
-import 'package:where_to_go_today/src/features/auth/register/register_screen.dart';
+import 'package:provider/provider.dart';
 
 import 'main_screen.dart';
-// import 'main_screen_vm.dart';
+import 'main_screen_vm.dart';
 
 /// Рроут экрана [MainScreen]
 class MainScreenRoute extends MaterialPage<void> {
-  const MainScreenRoute()
+  MainScreenRoute()
       : super(
-          child: const RegisterScreen(),
-          // TODO(Dmitry Dudunov): убрать при появлении авторизации
-          // Provider<MainScreenVm>(
-          //   create: MainScreenVm.new,
-          //   child: Builder(
-          //     builder: (context) => MainScreen(
-          //       vm: context.read<MainScreenVm>(),
-          //     ),
-          //   ),
-          // ),
+          child: Provider<MainScreenVm>(
+            create: MainScreenVm.new,
+            child: Builder(
+              builder: (context) => MainScreen(
+                vm: context.read<MainScreenVm>(),
+              ),
+            ),
+          ),
         );
 }
