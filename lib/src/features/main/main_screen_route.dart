a9c2dd1a7043658e10f3899b5de4ace18c0efd9c
Dmitry Dudunov <dydynovdima6@gmail.com>
2022-01-29 21:07:52 +0300

    fix: main_screen

--- lib/src/features/main/main_screen_route.dart
+++ lib/src/features/main/main_screen_route.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
-import 'package:where_to_go_today/src/features/auth/code/code_screen.dart';
 
 import 'main_screen.dart';
 import 'main_screen_vm.dart';
@@ -9,14 +8,13 @@ import 'main_screen_vm.dart';
 class MainScreenRoute extends MaterialPage<void> {
   MainScreenRoute()
       : super(
-          child: const CodeScreen(),
-          // Provider<MainScreenVm>(
-          //   create: MainScreenVm.new,
-          //   child: Builder(
-          //     builder: (context) => MainScreen(
-          //       vm: context.read<MainScreenVm>(),
-          //     ),
-          //   ),
-          // ),
+          child: Provider<MainScreenVm>(
+            create: MainScreenVm.new,
+            child: Builder(
+              builder: (context) => MainScreen(
+                vm: context.read<MainScreenVm>(),
+              ),
+            ),
+          ),
         );
 }
