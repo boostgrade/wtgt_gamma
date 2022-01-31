e5ae0acc6b550a5824902432d78c8787de4c2177
Dmitriy Usatov <usatov.dv@gmail.com>
2022-02-01 00:10:08 +0300

    feat: AuthBloc init

--- lib/src/features/auth/services/bloc/auth_event.dart
+++ lib/src/features/auth/services/bloc/auth_event.dart
@@ -1,1 +1,29 @@
-class AuthEvent {}
+abstract class AuthEvent {}
+
+class AuthEventLoginFacebook extends AuthEvent {}
+
+class AuthEventLoginVkontakte extends AuthEvent {}
+
+class AuthEventLoginGoogle extends AuthEvent {}
+
+class AuthEventSendSms extends AuthEvent {
+  final String phone;
+
+  AuthEventSendSms(this.phone);
+}
+
+class AuthEventReSendSms extends AuthEvent {}
+
+class AuthEventRegisterUser extends AuthEvent {
+  final String name;
+  final String surname;
+  final String email;
+  final DateTime birthdate;
+
+  AuthEventRegisterUser({
+    required this.name,
+    required this.surname,
+    required this.email,
+    required this.birthdate,
+  });
+}
--- lib/src/features/auth/services/bloc/auth_state.dart
+++ lib/src/features/auth/services/bloc/auth_state.dart
@@ -1,1 +1,13 @@
-class AuthState {}
+abstract class AuthState {}
+
+class AuthStateIdle extends AuthState {}
+
+class AuthStateInProgress extends AuthState {}
+
+class AuthStateSuccess extends AuthState {}
+
+class AuthStateError extends AuthState {
+  final dynamic error;
+
+  AuthStateError(this.error);
+}
