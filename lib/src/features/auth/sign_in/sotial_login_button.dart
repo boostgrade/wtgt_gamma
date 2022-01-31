8ef45df4f874eac29375c2d9382d214a0b2e881c
Dmitriy <usatov.dv@gmail.com>
2022-01-31 20:56:42 +0300

    feat: sign_in_screen (#33)
    * feat: sign_in_screen
    
    * fix: updated CHANGELOG.md
    
    * fix: after PR comments
    
    * fix: added icons
    
    * fix: improved SignInScreen layout
    
    * fix: improved SignInScreen layout
    
    * fix: fix CHANGELOG.md
    
    * fix: improved SignInScreen layout
    
    * fix: after main merge

---
+++ lib/src/features/auth/sign_in/sotial_login_button.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+
+class SotialLoginButton extends StatelessWidget {
+  final String imageAsset;
+  final VoidCallback? onPressed;
+
+  const SotialLoginButton({
+    Key? key,
+    required this.imageAsset,
+    this.onPressed,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return IconButton(
+      padding: EdgeInsets.zero,
+      splashRadius: 24,
+      onPressed: onPressed,
+      icon: SvgPicture.asset(
+        imageAsset,
+        width: 48,
+        height: 48,
+      ),
+    );
+  }
+}
