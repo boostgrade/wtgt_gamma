6641935b3cc4780d053ad7866fd744a23ab00109
loopnyapy <da.eto.deniska@gmail.com>
2022-02-08 15:31:54 +0300

    fix: auth_repository_test directory

--- test/src/features/authservices/repository/auth_repository_test.dart
+++ test/src/features/authservices/repository/auth_repository_test.dart
@@ -183,8 +183,7 @@ Future<void> main() async {
     });
 
     test('Метод Регистрация', () async {
-      when(() => apiMock.register(any()))
-          .thenAnswer((_) async => Future.value());
+      when(() => apiMock.register(any())).thenAnswer((_) async => Future.value());
 
       final registerRequest = RegisterRequest(
         name: '',
