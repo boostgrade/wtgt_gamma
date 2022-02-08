d3c96448def6d534ef22eedf239959647c1f8992
loopnyapy <da.eto.deniska@gmail.com>
2022-02-08 15:40:37 +0300

    fix: flutter format

--- test/src/features/authservices/repository/auth_repository_test.dart
+++ test/src/features/authservices/repository/auth_repository_test.dart
@@ -183,7 +183,9 @@ Future<void> main() async {
     });
 
     test('Метод Регистрация', () async {
-      when(() => apiMock.register(any())).thenAnswer((_) async => Future.value());
+      when(() => apiMock.register(any())).thenAnswer(
+        (_) async => Future.value(),
+      );
 
       final registerRequest = RegisterRequest(
         name: '',
