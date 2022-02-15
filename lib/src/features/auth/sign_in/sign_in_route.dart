93978e8fb1d20e57038efa2da53bed504a19663c
loopnyapy <da.eto.deniska@gmail.com>
2022-02-15 20:45:45 +0300

    feat: SignInScreenVm feat: SignInRoute feat: SignInScreen update to use ViewModel fix: SotialIconButton misspell feat: initialRoute change to SignInRoute for test in AppRouter feat: new AuthorizationException feat: use of new exception in SnackBarErrorScenarios feat: update AppDependency feat: exception mixin in AuthBloc fix: auth_bloc_test constructor

---
+++ lib/src/features/auth/sign_in/sign_in_route.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:where_to_go_today/src/di/app_dependencies.dart';
+import 'package:where_to_go_today/src/features/auth/sign_in/sign_in_screen.dart';
+import 'package:where_to_go_today/src/features/auth/sign_in/sign_in_screen_vm.dart';
+
+/// Маршрут для навигации к экрану авторизации
+class SignInRoute extends MaterialPage<void> {
+  static const routeName = '/signIn';
+
+  SignInRoute()
+      : super(
+          child: Provider<SignInScreenVm>(
+            create: (ctx) => SignInScreenVm(
+              ctx.read<AppDependencies>().authBloc,
+              errorHandler: ctx.read<AppDependencies>().errorHandler,
+              context: ctx,
+            ),
+            child: Builder(
+              builder: (context) {
+                return SignInScreen(
+                  vm: context.read<SignInScreenVm>(),
+                );
+              },
+            ),
+          ),
+        );
+}
