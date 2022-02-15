a5eec13eba20041e6fac61c8c73b2485249737dc
Dmitry Dudunov <dydynovdima6@gmail.com>
2022-02-15 19:26:16 +0300

    init: facebook

---
+++ lib/src/features/auth/services/facebook/facebook_auth.dart
@@ -0,0 +1,11 @@
+class FacebookAuth {
+  Future<void> login() async {
+    // ignore: avoid_print
+    print('');
+  }
+
+  Future<void> logout() async {
+    // ignore: avoid_print
+    print('');
+  }
+}
