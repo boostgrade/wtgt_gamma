f7df728c9e1f26ed9f2bc61d398d483655dff716
Dmitry Dudunov <dydynovdima6@gmail.com>
2022-03-25 14:54:45 +0300

    fix: remove mockito package

--- test/auth/register_screen/register_screen_test.dart
+++ test/auth/register_screen/register_screen_test.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_localizations/flutter_localizations.dart';
 import 'package:golden_toolkit/golden_toolkit.dart';
-import 'package:mockito/mockito.dart';
+import 'package:mocktail/mocktail.dart';
 import 'package:where_to_go_today/src/core/ui/errors_handling/scenario_error_handler/scenario_error_handler.dart';
 import 'package:where_to_go_today/src/features/auth/register/register_screen.dart';
 import 'package:where_to_go_today/src/features/auth/register/register_screen_vm.dart';
