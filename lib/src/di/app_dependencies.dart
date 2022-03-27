7daf2339975315115b7df84aad16dc92497f3ab2
Dmitriy Usatov <usatov.dv@gmail.com>
2022-03-27 20:21:55 +0300

    fix: after merge

--- lib/src/di/app_dependencies.dart
+++ lib/src/di/app_dependencies.dart
@@ -1,25 +1,22 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:where_to_go_today/src/core/services/network/dio/dio_module.dart';
+import 'package:where_to_go_today/src/core/services/network/firebase_options.dart';
 import 'package:where_to_go_today/src/core/ui/errors_handling/scenario_error_handler/scenario_error_handler.dart';
 import 'package:where_to_go_today/src/core/ui/errors_handling/scenario_error_handler/scenarios/snackbar_error_scenarios.dart';
 import 'package:where_to_go_today/src/core/ui/messages/default_message_controller.dart';
+import 'package:where_to_go_today/src/di/base/dependency_bundle.dart';
 import 'package:where_to_go_today/src/features/auth/services/auth_bloc.dart';
 import 'package:where_to_go_today/src/features/auth/services/google/google_auth.dart';
 import 'package:where_to_go_today/src/features/auth/services/storage/token_storage.dart';
 import 'package:where_to_go_today/src/features/auth/services/vk/vk_auth.dart';
 import 'package:where_to_go_today/src/features/authservices/api/auth_api.dart';
 import 'package:where_to_go_today/src/features/authservices/repository/auth_repository.dart';
-import 'package:where_to_go_today/src/features/onboard/services/onboarding_bloc.dart';
 import 'package:where_to_go_today/src/features/main/places/service/places_bloc.dart';
 import 'package:where_to_go_today/src/features/main/places/service/repository/places_repository.dart';
+import 'package:where_to_go_today/src/features/onboard/services/onboarding_bloc.dart';
 import 'package:where_to_go_today/src/features/settings/service/event/settings_event.dart';
 import 'package:where_to_go_today/src/features/settings/service/repository/settings_repository.dart';
 import 'package:where_to_go_today/src/features/settings/service/settings_bloc.dart';
-
-import '../core/services/network/firebase_options.dart';
-import '../features/authservices/api/auth_api.dart';
-import '../features/authservices/repository/auth_repository.dart';
-import 'base/dependency_bundle.dart';
 
 /// Класс с глобальными зависимостями приложения
 /// Здесь будем описывать то, что является синглтонами.
--- lib/src/navigation/app_router.dart
+++ lib/src/navigation/app_router.dart
@@ -2,9 +2,8 @@ import 'package:routemaster/routemaster.dart';
 import 'package:where_to_go_today/src/features/auth/code/code_route.dart';
 import 'package:where_to_go_today/src/features/auth/register/register_route.dart';
 import 'package:where_to_go_today/src/features/auth/sign_in/sign_in_route.dart';
-import 'package:where_to_go_today/src/features/main/main_screen_route.dart';
-import 'package:where_to_go_today/src/features/onboard/ui/onboarding_route.dart';
 import 'package:where_to_go_today/src/features/main/main_route.dart';
+import 'package:where_to_go_today/src/features/onboard/ui/onboarding_route.dart';
 import 'package:where_to_go_today/src/features/place_detail/place_detail_route.dart';
 import 'package:where_to_go_today/src/features/settings/ui/settings_route.dart';
 
@@ -20,7 +19,7 @@ class AppRouter {
       initialRoute: (_) => MainRoute(),
       SignInRoute.routeName: (_) => SignInRoute(),
       OnboardingRoute.routeName: (_) => OnboardingRoute(),
-      RegisterRoute.routeName: (_) => const RegisterRoute(),
+      RegisterRoute.routeName: (_) => RegisterRoute(),
       '${CodeRoute.routeName}/:phone': (routeData) =>
           CodeRoute(routeData.pathParameters['phone'] ?? ''),
       SettingsRoute.routeName: (_) => SettingsRoute(),
