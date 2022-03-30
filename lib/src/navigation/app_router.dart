30c6f062418a558ac3966aa59fcc37a8c6beb424
Dmitriy Usatov <usatov.dv@gmail.com>
2022-03-30 19:10:19 +0300

    fix: after merge

--- lib/src/navigation/app_router.dart
+++ lib/src/navigation/app_router.dart
@@ -18,7 +18,6 @@ class AppRouter {
     routes: {
       initialRoute: (_) => MainRoute(),
       SignInRoute.routeName: (_) => SignInRoute(),
-      MainScreenRoute.routeName: (_) => MainScreenRoute(),
       OnboardingRoute.routeName: (_) => OnboardingRoute(),
       RegisterRoute.routeName: (_) => RegisterRoute(),
       '${CodeRoute.routeName}/:phone': (routeData) =>
