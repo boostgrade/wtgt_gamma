a6db93a2e0d675ad7a2ec2711cd6a539fd08e1d7
Dmitriy Usatov <usatov.dv@gmail.com>
2022-04-08 22:17:41 +0300

    fix: after merge

--- lib/src/di/app_dependencies.dart
+++ lib/src/di/app_dependencies.dart
@@ -12,17 +12,15 @@ import 'package:where_to_go_today/src/features/auth/services/storage/token_stora
 import 'package:where_to_go_today/src/features/auth/services/vk/vk_auth.dart';
 import 'package:where_to_go_today/src/features/authservices/api/auth_api.dart';
 import 'package:where_to_go_today/src/features/authservices/repository/auth_repository.dart';
+import 'package:where_to_go_today/src/features/location/location_service.dart';
+import 'package:where_to_go_today/src/features/main/places/service/places_bloc.dart';
+import 'package:where_to_go_today/src/features/main/places/service/repository/places_repository.dart';
 import 'package:where_to_go_today/src/features/onboard/services/onboarding_bloc.dart';
 import 'package:where_to_go_today/src/features/onboard/services/repository/onboard_repository.dart';
 import 'package:where_to_go_today/src/features/onboard/services/storage/onboard_storage.dart';
 import 'package:where_to_go_today/src/features/settings/service/event/settings_event.dart';
 import 'package:where_to_go_today/src/features/settings/service/repository/settings_repository.dart';
 import 'package:where_to_go_today/src/features/settings/service/settings_bloc.dart';
-
-import '../core/services/network/firebase_options.dart';
-import '../features/authservices/api/auth_api.dart';
-import '../features/authservices/repository/auth_repository.dart';
-import 'base/dependency_bundle.dart';
 
 /// Класс с глобальными зависимостями приложения
 /// Здесь будем описывать то, что является синглтонами.
--- lib/src/features/main/profile/profile_screen_widget.dart
+++ lib/src/features/main/profile/profile_screen_widget.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
+import 'package:where_to_go_today/src/di/app_dependencies.dart';
 import 'package:where_to_go_today/src/features/main/profile/profile_screen.dart';
 import 'package:where_to_go_today/src/features/main/profile/profile_vm.dart';
 
@@ -9,7 +10,9 @@ class ProfileScreenWidget extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Provider<ProfileVm>(
-      create: (_) => ProfileVm(),
+      create: (_) => ProfileVm(
+        errorHandler: context.read<AppDependencies>().errorHandler,
+      ),
       child: Builder(
         builder: (context) => ProfileScreen(
           vm: context.read<ProfileVm>(),
